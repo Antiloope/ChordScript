c060c09a5e736bb44d884b081e2aa463de5db6ba
Antiloope <rodrigopizarro1234@gmail.com>
2020-06-26 19:18:28 -0300

    Added map to store dataTypes and variables

--- interpteter/context.cpp
+++ interpteter/context.cpp
@@ -1,6 +1,38 @@
 #include "context.h"
 
+using namespace CS;
+
+Context* Context::_instance = nullptr;
+
 Context::Context()
 {
+    _dataTypes = {
+        {"sample","sample"},
+        {"sound","sound"},
+        {"real","real"},
+        {"integer","integer"},
+        {"argument","argument"},
+        {"group","group"},
+        {"boolean","boolean"},
+        {"buffer","buffer"},
+        {"string","string"},
+    };
 
+    _variableNames = {
+        {"null","null"},
+    };
+}
+
+bool Context::isDataType(string name){
+    return _dataTypes.find(name) != _dataTypes.end();
+}
+
+bool Context::nameExist(string name){
+    return _variableNames.find(name) != _variableNames.end();
+}
+
+Context* Context::getInstance()
+{
+    if ( !_instance ) _instance = new Context();
+    return _instance;
 }
--- interpteter/context.h
+++ interpteter/context.h
@@ -1,11 +1,25 @@
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+namespace CS {
 
 class Context
 {
 public:
+    static Context* getInstance();
+    bool isDataType(string);
+    bool nameExist(string);
+private:
+    static Context* _instance;
     Context();
+    unordered_map<string,string> _dataTypes;
+    unordered_map<string,string> _variableNames;
 };
+
+}
 
 #endif // CONTEXT_H
