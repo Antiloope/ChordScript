dd1fd3f9c542d4ec58157df0a77e9c81ce34e7c2
Antiloope <rodrigopizarro1234@gmail.com>
2020-06-27 18:31:51 -0300

    Finished instruction constructor

--- interpteter/nonterminalexpressions.cpp
+++ interpteter/nonterminalexpressions.cpp
@@ -1,39 +1,42 @@
 #include "nonterminalexpressions.h"
+#include "utils/Exceptions/exception.h"
+#include "context.h"
 
 using namespace CS;
 
-NonTerminalExpression::NonTerminalExpression() : Expression() {
+NonTerminalExpression::NonTerminalExpression() : Expression() {}
 
-}
-NonTerminalExpression::NonTerminalExpression(size_t codeReference) : Expression(codeReference) {
+NonTerminalExpression::NonTerminalExpression(size_t codeReference) : Expression(codeReference) {}
 
-}
-NonTerminalExpression::~NonTerminalExpression(){
-
-}
+NonTerminalExpression::~NonTerminalExpression(){}
 
 /// Program expression
 
 ProgramExpression::ProgramExpression(list<TerminalExpression*> expressionsList) : NonTerminalExpression(0){
-    expressionsList.pop_front();
-    expressionsList.pop_front();
-    expressionsList.pop_front();
-}
-
-ProgramExpression::ProgramExpression(list<TerminalExpression*>* expressionsList) : NonTerminalExpression(0){
-
+    while (!expressionsList.empty())
+    {
+        _instructionsList.push_back(new InstructionExpression(&expressionsList,expressionsList.front()->getCodeReference()));
+    }
 }
 
 ProgramExpression::ProgramExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){
-
+    TerminalExpression* tmp = expressionsList->front();
+    while (
+        (typeid (*tmp).hash_code() != typeid (ReturnExpression).hash_code() &&
+         typeid (*tmp).hash_code() != typeid (CloseBraceExpression).hash_code()) &&
+        !expressionsList->empty())
+    {
+        _instructionsList.push_back(new InstructionExpression(expressionsList,tmp->getCodeReference()));
+        tmp = expressionsList->front();
+    }
 }
 
 ProgramExpression::~ProgramExpression(){
     InstructionExpression* tmp;
-    while(!instructionsList.empty())
+    while(!_instructionsList.empty())
     {
-        tmp = instructionsList.front();
-        instructionsList.pop_front();
+        tmp = _instructionsList.front();
+        _instructionsList.pop_front();
         delete tmp;
     }
 }
@@ -41,3 +44,152 @@ ProgramExpression::~ProgramExpression(){
 void ProgramExpression::interpret(){
 
 }
+
+/// Instruction expression
+
+InstructionExpression::InstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){
+    if(expressionsList->empty()) throw SyntaxException("Expected another instruction",codeReference);
+
+    TerminalExpression* tmp = expressionsList->front();
+
+    if(typeid (*tmp).hash_code() == typeid (ForExpression).hash_code()){
+        _instruction = new ForInstructionExpression(expressionsList,codeReference);
+    }
+    else if(typeid (*tmp).hash_code() == typeid (IfExpression).hash_code()){
+        _instruction = new IfInstructionExpression(expressionsList,codeReference);
+    }
+    else if(typeid (*tmp).hash_code() == typeid (BreakExpression).hash_code()){
+        _instruction = new BreakInstructionExpression(expressionsList,codeReference);
+    }
+    else if(typeid (*tmp).hash_code() == typeid (EOEExpression).hash_code()){
+        _instruction = new EOEInstructionExpression(expressionsList,codeReference);
+    }
+    else if(typeid (*tmp).hash_code() == typeid (NameExpression).hash_code()){
+        NameExpression* nameExpression = (NameExpression*)tmp;
+        if(Context::getInstance()->isDataType(nameExpression->getName())){
+            auto it = expressionsList->begin();
+            advance(it,1);
+            if(it == expressionsList->end()) throw SyntaxException("Expected variable name",codeReference);
+            tmp = *it;
+            if( typeid (*tmp).hash_code() == typeid (NameExpression).hash_code() &&
+                Context::getInstance()->isValidName(((NameExpression*)tmp)->getName())){
+                advance(it,1);
+                if(it == expressionsList->end()) throw SyntaxException("Expected another symbol",tmp->getCodeReference());
+                tmp = *it;
+                if(typeid (*tmp).hash_code() == typeid (EqualExpression).hash_code()){
+                    _instruction = new AssignationExpression(expressionsList,codeReference);
+                }
+                else if(
+                    typeid (*tmp).hash_code() == typeid (EOEExpression).hash_code() ||
+                    typeid (*tmp).hash_code() == typeid (OpenParenthesisExpression).hash_code()){
+                    _instruction = new DefinitionExpression(expressionsList,codeReference);
+                }
+                else{
+                    throw SyntaxException("Expected another symbol",tmp->getCodeReference());
+                }
+            }
+            else{
+                throw SemanticException("Invalid name",tmp->getCodeReference());
+            }
+        }
+        else if(Context::getInstance()->nameExist(nameExpression->getName())){
+            auto it = expressionsList->begin();
+            advance(it,1);
+            if(it == expressionsList->end()) throw SyntaxException("Expected variable name",codeReference);
+            tmp = *it;
+            if( typeid (*tmp).hash_code() == typeid (EqualExpression).hash_code()){
+                _instruction = new AssignationExpression(expressionsList,codeReference);
+            }
+            else if(
+                typeid (*tmp).hash_code() == typeid (MemberAccessExpression).hash_code() ||
+                typeid (*tmp).hash_code() == typeid (OpenParenthesisExpression).hash_code()){
+                _instruction = new ExecutionExpression(expressionsList,codeReference);
+            }
+            else{
+                throw SyntaxException("Expected another symbol",codeReference);
+            }
+        }else{
+            throw SyntaxException("Unrecognized variable name or data type",codeReference);
+        }
+    }
+    else{
+        throw SyntaxException("Expected another instruction",codeReference);
+    }
+}
+
+InstructionExpression::~InstructionExpression(){
+    delete _instruction;
+}
+
+void InstructionExpression::interpret(){
+
+}
+
+/// For loop expression
+
+ForInstructionExpression::ForInstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){
+}
+
+ForInstructionExpression::~ForInstructionExpression(){
+
+}
+
+void ForInstructionExpression::interpret(){
+
+}
+
+/// If expression
+
+IfInstructionExpression::IfInstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){
+}
+
+IfInstructionExpression::~IfInstructionExpression(){
+
+}
+
+void IfInstructionExpression::interpret(){
+
+}
+
+/// Break expression
+
+BreakInstructionExpression::BreakInstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){
+}
+
+BreakInstructionExpression::~BreakInstructionExpression(){}
+
+void BreakInstructionExpression::interpret(){
+
+}
+
+/// EOE expression
+
+EOEInstructionExpression::EOEInstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){}
+
+EOEInstructionExpression::~EOEInstructionExpression(){}
+
+void EOEInstructionExpression::interpret(){}
+
+/// Assignation expression
+
+AssignationExpression::AssignationExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){}
+
+AssignationExpression::~AssignationExpression(){}
+
+void AssignationExpression::interpret(){}
+
+/// Definition expression
+
+DefinitionExpression::DefinitionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){}
+
+DefinitionExpression::~DefinitionExpression(){}
+
+void DefinitionExpression::interpret(){}
+
+/// Execution expression
+
+ExecutionExpression::ExecutionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference) : NonTerminalExpression(codeReference){}
+
+ExecutionExpression::~ExecutionExpression(){}
+
+void ExecutionExpression::interpret(){}
--- interpteter/nonterminalexpressions.h
+++ interpteter/nonterminalexpressions.h
@@ -13,23 +13,72 @@ public:
     virtual void interpret() = 0;
 };
 
+class DefinitionExpression : public NonTerminalExpression {
+public:
+    DefinitionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
+    ~DefinitionExpression();
+    void interpret() override;
+};
+
+class AssignationExpression : public NonTerminalExpression {
+public:
+    AssignationExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
+    ~AssignationExpression();
+    void interpret() override;
+};
+
+class ExecutionExpression : public NonTerminalExpression {
+public:
+    ExecutionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
+    ~ExecutionExpression();
+    void interpret() override;
+};
+
+class ForInstructionExpression : public NonTerminalExpression {
+public:
+    ForInstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
+    ~ForInstructionExpression();
+    void interpret() override;
+};
+
+class IfInstructionExpression : public NonTerminalExpression {
+public:
+    IfInstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
+    ~IfInstructionExpression();
+    void interpret() override;
+};
+
+class BreakInstructionExpression : public NonTerminalExpression {
+public:
+    BreakInstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
+    ~BreakInstructionExpression();
+    void interpret() override;
+};
+
+class EOEInstructionExpression : public NonTerminalExpression {
+public:
+    EOEInstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
+    ~EOEInstructionExpression();
+    void interpret() override;
+};
+
 class InstructionExpression : public NonTerminalExpression {
 public:
-    InstructionExpression(list<TerminalExpression*>* expressionsList);
     InstructionExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
     ~InstructionExpression();
     void interpret() override;
+private:
+    NonTerminalExpression* _instruction;
 };
 
 class ProgramExpression : public NonTerminalExpression {
 public:
     ProgramExpression(list<TerminalExpression*> expressionsList);
-    ProgramExpression(list<TerminalExpression*>* expressionsList);
     ProgramExpression(list<TerminalExpression*>* expressionsList, size_t codeReference);
     ~ProgramExpression();
     void interpret() override;
 private:
-    list<InstructionExpression*> instructionsList;
+    list<InstructionExpression*> _instructionsList;
 };
 
 }
